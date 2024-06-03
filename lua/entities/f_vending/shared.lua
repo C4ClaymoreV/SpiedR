74e01a4bf8d5da3481a4f317773328824a547f99
C4ClaymoreV <63988663+C4ClaymoreV@users.noreply.github.com>
2024-06-02 22:13:51 -0400

    updated shared.lua

--- lua/entities/f_vending/shared.lua
+++ lua/entities/f_vending/shared.lua
@@ -3,7 +3,7 @@ ENT.Type = "anim" -- Sets the entity type to 'anim', indicating it's an animated
 ENT.Base = "base_gmodentity" -- Specifies that this entity is based on the 'base_gmodentity', inheriting its functionality.
 ENT.PrintName = "Flof's Vending" -- The name that will appear in the spawn menu.
 ENT.Author = "Flof" -- The author's name for this entity.
-ENT.Category = "Test entities" -- The category for this entity in the spawn menu.
+ENT.Category = "DarkRP Content" -- The category for this entity in the spawn menu.
 ENT.Contact = "FloffenWaffles" -- The contact details for the author of this entity.
 ENT.Purpose = "To test the creation of entities." -- The purpose of this entity.
 ENT.Spawnable = true -- Specifies whether this entity can be spawned by players in the spawn menu.
@@ -14,19 +14,6 @@ local slotWhitelist = {
     ammo_pistol = true,
     ammo_ar2    = true
 }
-
-concommand.Add("vend_whitelist_add", function(ply, _, args)
-
-    if not ply:IsSuperAdmin() then return end
-        
-    if slotWhitelist[args[1]] == true then
-        ply:ChatPrint( args[1] .. " is already on the whitelist!" )
-        return
-    end
-    slotWhitelist[args[1]] = true
-    ply:ChatPrint( args[1] .. " has been added to vending whitelist!" )
-
-end)
 
 if SERVER then
 
