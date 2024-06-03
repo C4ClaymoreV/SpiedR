6a57c5af77344c02795ede955a173f4eb0faf88d
C4ClaymoreV <63988663+C4ClaymoreV@users.noreply.github.com>
2024-06-03 02:47:05 -0400

    added schizophrenia

--- lua/entities/f_vending/cl_init.lua
+++ lua/entities/f_vending/cl_init.lua
@@ -1,6 +1,57 @@
 include("shared.lua")
 
--- Client-side draw function for the entity
+local buttons = {}
+local button = {
+    register = function(panel, index, x, y, w, h, col, text, func)
+        buttons[panel] = buttons[panel] or {}
+        buttons[panel][index] = {x= x, y= y, w= w, h= h, text= text, func= func}
+    end,
+
+    draw = function(panel, phase, index, col)
+        if index then 
+            
+            local b = buttons[panel][index]
+            surface.SetDrawColor(col[1], col[2], col[3], phase)
+            surface.DrawRect(b.x, b.y, b.w, b.h)
+
+            surface.SetDrawColor(255, 255, 255, phase)
+            surface.DrawOutlinedRect(b.x, b.y, b.w, b.h)
+
+            draw.DrawText(b.text, nil, b.x + b.w/2, b.y + b.h/2 - 8, Color(255, 255, 255, phase), TEXT_ALIGN_CENTER)
+
+        else -- too lazy to be clever
+            for i, b in pairs(buttons[panel]) do
+
+                surface.SetDrawColor(col[1], col[2], col[3], phase)
+                surface.DrawRect(b.x, b.y, b.w, b.h)
+
+                surface.SetDrawColor(255, 255, 255, phase)
+                surface.DrawOutlinedRect(b.x, b.y, b.w, b.h)
+
+                draw.DrawText(b.text, nil, b.x + b.w/2, b.y + b.h/2 - 8, Color(255, 255, 255, phase), TEXT_ALIGN_CENTER)
+
+            end
+        end
+    end,
+
+    findInside = function(panel, pos)
+        for i, v in pairs(buttons[panel]) do
+            if v.x < pos.x and v.x + v.w > pos.x and v.y < pos.y and v.y + v.h > pos.y then return i end
+        end
+    end,
+
+    doClick = function(panel, index)
+        buttons[panel][index].func()
+    end
+}
+
+for i = 1, 8 do -- button registeration
+    button.register("Main", i, 2, 2 + (i-1)*62, 80, 45, {50, 50, 50}, "test", function() print("you're so awesome") end)
+    button.register("Main", i + 8, 2, 2 + (i)*62 -15, 56, 13, {50, 50, 50}, "buy 1", function() print("you're so awesome") end)
+    button.register("Main", i + 16, 60, 2 + (i)*62 -15, 22, 13, {50, 50, 50}, "5x", function() print("you're so awesome") end)
+end
+
+local selected = {}
 
 function ENT:Draw()
     self:DrawModel()
@@ -17,11 +68,27 @@ function ENT:Draw()
 		surface.DrawRect (0, 0, 85,500)
 	
 		surface.SetDrawColor(255, 255, 255, phaseA)
-		surface.DrawOutlinedRect ( 0, 0, 85, 500)
-		
-		surface.SetTextColor(255, 255, 255, phaseA)
-		surface.SetTextPos( 4, 4 ) 
-		surface.DrawText( "KYS" ) 
-		
+		surface.DrawOutlinedRect( 0, 0, 85, 500)
+
+        if dist > 16000 then cam.End3D2D() return end -- too far away to select the screen
+
+        local ftrace = util.IntersectRayWithPlane( EyePos(), EyeVector(), self:LocalToWorld( Vector(17.5, 17.5, 32) ), self:LocalToWorldAngles( Angle(0, 90, 90)):Up() )
+        if ftrace == nil then cam.End3D2D() return end
+        ftrace = self:WorldToLocal(ftrace) - Vector()
+        c_pos = { x = (ftrace[2] - 17.5) * 10, y = (-ftrace[3] + 32) * 10 }
+
+        --if c_pos.x < -10 or c_pos.x > 95 or c_pos.y < -10 or c_pos.y > 510 then cam.End3D2D() return end -- outer bounds disable further trace functions
+
+        local sel = button.findInside("Main", c_pos) or 0
+        selected = {"Main", sel}
+
+        for i, v in pairs(buttons["Main"]) do
+            if i ~= sel then
+                button.draw("Main", phasea, i, {50, 50, 50})
+            else         
+                button.draw("Main", phasea, sel, {100, 100, 100})
+            end
+        end
+
 	cam.End3D2D()
 end
